a25d0e50023c4df36298fb934f2846771936e3b8
Tanapol Wong-asa <tanapol@skooldio.com>
2023-12-14 19:19:31 +0700

    initial

---
+++ tailwind.config.js
@@ -0,0 +1,17 @@
+tailwind.config = {
+  content: ["./src/**/*.{html,js}"],
+  theme: {
+    extend: {
+      // you can add configuration (customization) here
+      colors: {
+        primary: "#DEF81D",
+        "wd-white": "#FFFFFF",
+        "wd-grey": "#999999",
+        "wd-darkgrey": "#2D2D2D",
+        "wd-black": "#080808",
+      },
+    },
+  },
+};
+
+module.export = tailwind.config;

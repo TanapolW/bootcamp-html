bb941894bae52b3accb5574a4e093102637ba842
Tanapol Wong-asa <korn_tanapol@outlook.co.th>
2023-12-17 18:18:18 +0700

    header, features, my work section

--- tailwind.config.js
+++ tailwind.config.js
@@ -10,6 +10,10 @@ tailwind.config = {
         "wd-darkgrey": "#2D2D2D",
         "wd-black": "#080808",
       },
+      backgroundImage: {
+        "hero-pattern":
+          "linear-gradient(to left, rgba(0,0,0, 0), rgba(0,0,0,1)), url('assets/hero.jpg')",
+      },
     },
   },
 };
